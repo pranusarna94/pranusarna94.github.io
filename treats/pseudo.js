498d1eb3719dcf6ca950a50f7cbed51ad4dd5707
Pranu Sarna <sarnapranu@gmail.com>
2018-03-30 19:04:14 +0530

    done stuff

--- treats/pseudo.js
+++ treats/pseudo.js
@@ -1,14 +1,40 @@
-//Logic for addition and removal of treats for non-members
+
+
+
+
+
+////////////////////////////////////////////////////////////
+//Logic for addition and removal of treats for non-members//
+////////////////////////////////////////////////////////////
 if(treats_subscription_added){
 	add_to_cart(treats_item);
 
 	if(treats_subscription_removed){
-		alert("Are you sure you want to remove the subscription? This will also remove the free dessert.");
+		alert("Are you sure you want to remove the subscription?");
 
 		if(alert()==yes){
 			remove_from_cart(treats_subscription && treats_item)
 		}else{
-			return
+			do_nothing()
 		}
+	}
+}
+
+
+
+/////////////////////////////////////////////////////////
+//Logic for addition and removal of treats for members//
+/////////////////////////////////////////////////////////
+if(in_cart(treats_item)){
+	if(remove_from_cart(treats_item)){
+		alert("Are you sure you want to remove the free item?");
+		if(alert()==yes)
+			remove_from_cart(treats_item);
+		else
+			do_nothing();
+	}
+
+	if(add_to_cart(treats_item)){
+		add_to_cart(treats_item);
 	}
 }
